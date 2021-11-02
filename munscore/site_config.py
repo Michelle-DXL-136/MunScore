f486f13ed3cab73574f1110e829349cc56547360
George Yu <yudumpling@gmail.com>
2021-11-02 14:23:55 +0800

    Update score names and default values

--- munscore/site_config.py
+++ munscore/site_config.py
@@ -1,6 +1,6 @@
 VENUES = ['A 会场', 'B 会场', 'C 会场']
 PARTIES = ['A 党', 'B 党']
 
-SCORE_NAME = {'contestant': '议员指数', 'party': '政府执行力指数', 'venue': '国会政治矛盾指数'}
-DEFAULT_SCORE = {'contestant': 100, 'party': 100, 'venue': 0}
+SCORE_NAME = {'contestant': '议员指数', 'party': '会议推进指数', 'venue': '谈判指数'}
+DEFAULT_SCORE = {'contestant': 50, 'party': 50, 'venue': 50}
 SCORE_DILATION = 5
